84a12270c13633421874e941e4d1b05ff038307b
GuilhermeReiis <guilhermegiordannygalo@gmail.com>
2025-01-14 13:08:13 -0300

    fixed

---
+++ src/main/entities/index.intities.ts
@@ -0,0 +1,7 @@
+export { CSFloat } from "./csfloat.entity";
+export { MarketCSGO } from "./marketcsgo.entity";
+export { Settings } from "./settings.entity";
+export { Shadowpay } from "./shadowpay.entity";
+export { User } from "./user.entity";
+export { UserSettings } from "./userSettings";
+export { Waxpeer } from "./waxpeer.entity";
--- src/main/services/db.ts
+++ src/main/services/db.ts
@@ -1,88 +1,26 @@
 import "reflect-metadata";
 import { infoLogger } from "../../shared/helpers";
 import { DataSource } from "typeorm";
-import { Settings } from "../entities/settings";
-import { exec } from "child_process";
+import { app } from "electron";
 import path from "path";
-import util from "util";
-import { CSFloat } from "../entities/csfloat.entity";
-import { MarketCSGO } from "../entities/marketcsgo.entity";
-import { Shadowpay } from "../entities/shadowpay.entity";
-import { User } from "../entities/user.entity";
-import { UserSettings } from "../entities/userSettings";
-import { Waxpeer } from "../entities/waxpeer.entity";
-import * as dotenv from "dotenv";
-import asar from "asar";
-import fs from "fs";
-import { app } from "electron"; // Certifique-se de que esta importação está correta e disponível
+import {
+  CSFloat,
+  MarketCSGO,
+  Settings,
+  Shadowpay,
+  User,
+  UserSettings,
+  Waxpeer,
+} from "../entities/index.intities";
 
-// Carrega as variáveis de ambiente
-dotenv.config();
-
-// Promisifica a execução de comandos
-const execAsync = util.promisify(exec);
-
-// Função para extrair migrações de app.asar
-const extractMigrations = () => {
-  const asarPath = path.join(__dirname, "app.asar");
-  const extractPath = path.join(__dirname, "extracted");
-
-  if (!fs.existsSync(asarPath)) {
-    console.error("Arquivo app.asar não encontrado:", asarPath);
-    process.exit(1);
-  }
-
-  if (!fs.existsSync(extractPath)) {
-    fs.mkdirSync(extractPath);
-  }
-
-  asar.extractAll(asarPath, extractPath);
-};
-
-// Chama a função de extração quando necessário
-if (process.env.NODE_ENV === "production") {
-  console.log("Production mode detected, extracting migrations...");
-  extractMigrations();
-}
-
-// Função para obter o caminho das migrações
-const getMigrationsPath = () => {
-  if (process.env.NODE_ENV === "production") {
-    return path.join(app.getPath("userData"), "migrations");
-  } else {
-    return path.resolve(__dirname, "../../migrations");
-  }
-};
-
-// Função para definir o caminho do banco de dados
-const getDBPath = () => {
+const getDBPath = (): string => {
   const basePath =
     process.env.NODE_ENV === "production"
       ? path.join(app.getPath("userData"), "db")
-      : path.resolve(__dirname, "../../../db");
-
-  console.log(
-    `==========getDBPath=========${process.env.NODE_ENV?.toUpperCase()}=`,
-    basePath
-  );
-
+      : path.resolve(__dirname, "../../db");
   return path.join(basePath, "db.sqlite");
 };
 
-// Função para definir o caminho da fonte de dados
-const getDataSourcePath = () => {
-  if (process.env.NODE_ENV === "production") {
-    console.log("==========getDataSourcePath=========PRODUCAO=");
-    return path.join(app.getAppPath(), "resources", "app.asar");
-  } else {
-    console.log("==========getDataSourcePath=========DEV=");
-    return path.resolve(__dirname, "../../../src/main/services/db.ts");
-  }
-};
-
-// Inicializa o caminho das migrações e configurações
-const migrationsPath = getMigrationsPath();
-const pathDB = getDBPath();
 const entities = [
   CSFloat,
   MarketCSGO,
@@ -93,102 +31,80 @@ const entities = [
   Waxpeer,
 ];
 
-// Configura o DataSource do TypeORM
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: pathDB,
-  synchronize: false,
+  database: getDBPath(),
+  synchronize: true,
   logging: process.env.NODE_ENV === "development",
-  entities: entities,
-  migrations: [
-    path.resolve(migrationsPath, "*.ts"), // Para desenvolvimento
-    path.resolve(migrationsPath, "*.js"), // Para produção
-  ],
+  entities,
   subscribers: [],
 });
 
 export class DB {
-  public dataSource: DataSource;
-  static instance: DB;
+  private static instance: DB;
+  private dataSource: DataSource;
 
-  constructor(dbPath: string) {
-    this.dataSource = AppDataSource.setOptions({
-      database: dbPath,
-    });
+  private constructor() {
+    this.dataSource = AppDataSource;
   }
 
-  // Método estático para iniciar o banco de dados
-  static async start() {
-    if (this.instance && this.instance.dataSource.isInitialized) {
+  static async start(): Promise<void> {
+    if (this.instance?.dataSource.isInitialized) {
       console.log(
         "Database is already initialized, skipping reinitialization."
       );
       return;
     }
 
-    const dbPath = getDBPath();
-    this.instance = new DB(dbPath);
+    this.instance = new DB();
+    console.log("==============================");
+    console.log("Entities:", entities);
+    console.log("==============================");
 
-    if (process.env.NODE_ENV === "test") {
-      console.log("Running in test mode, clearing database...");
-      const entities = this.instance.dataSource.entityMetadatas;
-      for (const entity of entities) {
-        const repository = this.instance.dataSource.getRepository(entity.name);
-        await repository.clear();
-      }
-      infoLogger("Database cleared!");
-    }
+    await this.initializeDatabase();
+  }
 
+  private static async initializeDatabase(): Promise<void> {
     try {
-      console.log("Initializing database...");
       await this.instance.dataSource.initialize();
-      console.log("Database initialized successfully.");
-    } catch (error: any) {
-      console.error("Error initializing database:", error.message);
-      console.error(error.stack);
-      process.exit(1);
-    }
+      console.log("Database initialized.");
 
-    // Verifica e executa migrações se necessário
-    try {
-      console.log("Running migrations...");
-      const dataSourcePath = getDataSourcePath();
-      console.log("DataSource Path:", dataSourcePath);
-
-      const hasMigrations = await this.instance.dataSource.showMigrations();
-      if (!hasMigrations) {
-        const command = `npx typeorm migration:run -d ${dataSourcePath}`;
-        const { stdout, stderr } = await execAsync(command);
-        if (stdout) console.log(stdout);
-        if (stderr) console.error(stderr);
-      } else {
-        console.log("Migrations are already up to date.");
+      if (process.env.NODE_ENV === "test") {
+        await this.clearDatabase();
       }
-    } catch (error: any) {
-      console.error("Error running migrations:", error.message);
-      console.error(error.stack);
-      process.exit(1);
-    }
 
-    // Configura as configurações do banco
-    try {
-      console.log("Setting up initial database configurations...");
-      const settings = await this.instance.dataSource
-        .getRepository(Settings)
-        .find();
-      if (settings.length === 0) {
-        const newSettings = new Settings();
-        await this.instance.dataSource
-          .getRepository(Settings)
-          .save(newSettings);
-        console.log("Default settings saved to database.");
-      }
-    } catch (error: any) {
-      console.error("Error setting up initial settings:", error.message);
+      await this.setupDefaultSettings();
+    } catch (error) {
+      console.error("Error during database initialization:", error.message);
       console.error(error.stack);
       process.exit(1);
     }
 
     console.log("Database setup complete!");
+  }
+
+  private static async clearDatabase(): Promise<void> {
+    console.log("Running in test mode, clearing database...");
+    const { entityMetadatas } = this.instance.dataSource;
+    for (const entity of entityMetadatas) {
+      const repository = this.instance.dataSource.getRepository(entity.name);
+      await repository.clear();
+    }
+    infoLogger("Database cleared!");
+  }
+
+  private static async setupDefaultSettings(): Promise<void> {
+    console.log("Setting up initial database configurations...");
+
+    const settingsRepository = this.instance.dataSource.getRepository(Settings);
+    const existingSettings = await settingsRepository.find();
+
+    if (existingSettings.length === 0) {
+      const defaultSettings = settingsRepository.create();
+      await settingsRepository.save(defaultSettings);
+      console.log("Default settings saved to database.");
+    } else {
+      console.log("Settings already exist in the database.");
+    }
   }
 }
