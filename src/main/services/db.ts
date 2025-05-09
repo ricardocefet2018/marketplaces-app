59d17d60e73ff84c6eb1df5622f20d7db00041cc
GuilhermeReiis <guilhermegiordannygalo@gmail.com>
2025-05-09 14:29:08 -0300

    Fix: Remove "console.logs"

--- src/main/services/db.ts
+++ src/main/services/db.ts
@@ -39,17 +39,9 @@ export class DB {
   }
 
   static async start(): Promise<void> {
-    if (this.instance?.dataSource.isInitialized) {
-      console.log(
-        "Database is already initialized, skipping reinitialization."
-      );
-      return;
-    }
+    if (this.instance?.dataSource.isInitialized) return;
 
     this.instance = new DB();
-    console.log("==============================");
-    console.log("Entities:", entities);
-    console.log("==============================");
 
     await this.initializeDatabase();
   }
@@ -57,24 +49,17 @@ export class DB {
   private static async initializeDatabase(): Promise<void> {
     try {
       await this.instance.dataSource.initialize();
-      console.log("Database initialized.");
 
-      if (process.env.NODE_ENV === "test") {
-        await this.clearDatabase();
-      }
+      if (process.env.NODE_ENV === "test") await this.clearDatabase();
 
       await this.setupDefaultSettings();
     } catch (error) {
-      console.error("Error during database initialization:", error.message);
       console.error(error.stack);
       process.exit(1);
     }
-
-    console.log("Database setup complete!");
   }
 
   private static async clearDatabase(): Promise<void> {
-    console.log("Running in test mode, clearing database...");
     const { entityMetadatas } = this.instance.dataSource;
     for (const entity of entityMetadatas) {
       const repository = this.instance.dataSource.getRepository(entity.name);
@@ -84,17 +69,14 @@ export class DB {
   }
 
   private static async setupDefaultSettings(): Promise<void> {
-    console.log("Setting up initial database configurations...");
-
     const settingsRepository = this.instance.dataSource.getRepository(Settings);
     const existingSettings = await settingsRepository.find();
 
     if (existingSettings.length === 0) {
       const defaultSettings = settingsRepository.create();
       await settingsRepository.save(defaultSettings);
-      console.log("Default settings saved to database.");
     } else {
-      console.log("Settings already exist in the database.");
+      infoLogger("Settings already exist in the database.");
     }
   }
 }
